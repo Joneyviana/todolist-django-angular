463b581cc7808ea032b7c5cceb46a6c81831ce5f
Joneyviana <arcistiel@gmail.com>
2016-08-24 17:24:38 -0300

    prepare for heroku

--- config/wsgi.py
+++ config/wsgi.py
@@ -28,7 +28,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(get_wsgi_application())
 '''
 if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
     application = Sentry(application)
