e12a309b1810a63e0e83fd98a076d8a23544c5c4
Joneyviana <arcistiel@gmail.com>
2016-08-24 16:38:09 -0300

    prepare for heroku

---
+++ staticfiles/js/app.js
@@ -0,0 +1,120 @@
+
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function($interpolateProvider) {
+    $interpolateProvider.startSymbol('{[');
+    $interpolateProvider.endSymbol(']}');
+  });
+app.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+}]);
+
+
+
+app.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+   .when('/:planName', {
+        templateUrl: '/static/templates/task_list.html',
+        controller: 'TaskService',
+     })
+   .otherwise({
+        templateUrl: '/static/templates/mylists.html',
+        controller: 'PlanListController',
+     });
+     
+});
+app.factory('Exibitplan', function() {
+    function ExibitMarkSelect(plan) { 
+        this.plan = plan;
+        var _self = this;
+       
+        this.markOffAll = function() {
+            for(task in _self.plan.tasks) {
+                
+              _self.plan.tasks[task].activate = false;
+            }
+        }
+        _self.markOffAll();
+        this.markItem = function(task){ 
+            _self.markOffAll();
+            task.activate = true;
+        }
+    }
+
+    return {
+        createNew: function(plan) {
+            return new ExibitMarkSelect(plan);
+        }
+    };
+});
+app.controller('TaskService', function($http,$scope,$routeParams,Exibitplan){  
+    var exibit ;
+    $scope.currentTask = "";
+    $scope.modEdit = false;
+    $http.get('api/plan/'+$routeParams.planName)
+    .success(function(data) {
+        exibit = Exibitplan.createNew(data);
+        $scope.plan = exibit.plan;
+    })
+  
+    $scope.adicionarTask = function(plan){
+        $http.post('api/task', {"name":plan.newTask,"plan":plan.id})
+        .success(function(data) {
+          plan.tasks.push(data)
+            plan.newTask = null;
+        })
+    }       
+    
+    $scope.selectTask = function(task){
+       exibit.markItem(task);
+       $scope.currentTask = task.name;
+    }
+    
+    $scope.removerTask = function(task,tasks){
+        $http.delete('api/task/'+task.id)
+        .success(function(data) {
+        var index = tasks.indexOf(task);
+            if (index != -1) {
+                tasks.splice(index, 1);
+            }
+        })
+    }
+    $scope.alterTask = function(task,text){
+        $http.patch('api/task/'+task.id, {"description":text})
+        .success(function(data) {
+            task.description = text;
+            $scope.modEdit = false;
+        })
+    } 
+});
+
+app.controller('PlanListController', function ($scope, $http) {
+    
+  if ($scope.plans === undefined) {
+  $http.get('api/plans')
+      .success(function(data) {
+     	   $scope.plans = data;
+   	})
+  }
+  
+  $scope.adicionarPlan = function(){
+        $http.post('api/plans', {"name":$scope.newPlan,"tasks":[]})
+   	    .success(function(data) {
+     	    $scope.plans.push(data)
+   	        $scope.newPlan = null;
+         })
+      }
+    
+   $scope.removerPlan = function(plan){
+       $http.delete('api/'+plan.id)
+   	    .success(function(data) {
+     		var index = $scope.plans.indexOf(plan);
+            if (index != -1) {
+                $scope.plans.splice(index, 1);
+               
+            }
+        })
+    }
+});
+
