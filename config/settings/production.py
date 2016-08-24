9aac2b86c8f32bf9685e12a0e5bbcb63406f55e1
Joneyviana <arcistiel@gmail.com>
2016-08-24 17:02:19 -0300

    prepare for heroku

--- config/settings/production.py
+++ config/settings/production.py
@@ -117,12 +117,13 @@ EMAIL_SUBJECT_PREFIX = env('DJANGO_EMAIL_SUBJECT_PREFIX', default='[todoList] ')
 SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
 
 # Anymail with Mailgun
+'''
 INSTALLED_APPS += ("anymail", )
 ANYMAIL = {
     "MAILGUN_API_KEY": env('DJANGO_MAILGUN_API_KEY'),
 }
 EMAIL_BACKEND = "anymail.backends.mailgun.MailgunBackend"
-
+'''
 # TEMPLATE CONFIGURATION
 # ------------------------------------------------------------------------------
 # See:
