25660350daa287c2179256412851abca307a01f7
Joneyviana <arcistiel@gmail.com>
2016-08-24 17:42:49 -0300

    prepare for heroku

--- config/wsgi.py
+++ config/wsgi.py
@@ -16,8 +16,7 @@ framework.
 import os
 
 from django.core.wsgi import get_wsgi_application
-if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
-    from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
@@ -25,12 +24,8 @@ if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
 # os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.production"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(get_wsgi_application())
+application = get_wsgi_application()
 '''
 if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
     application = Sentry(application)
