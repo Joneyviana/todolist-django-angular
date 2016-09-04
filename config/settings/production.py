b51af34ad71b22ba4e9675036e72c03a983c27ed
Joneyviana <arcistiel@gmail.com>
2016-09-04 20:39:49 -0300

    alterconfig

--- config/settings/production.py
+++ config/settings/production.py
@@ -64,7 +64,7 @@ X_FRAME_OPTIONS = 'DENY'
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['beautiylist.herokuapp.com',u'localhost'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['beautiylist.herokuapp.com','localhost'])
 # END SITE CONFIGURATION
 SERVER_EMAIL = 'joneyviana@gmail.com'
 INSTALLED_APPS += ('gunicorn', )
