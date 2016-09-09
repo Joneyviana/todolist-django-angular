653706a23a6ff8550bfb94a9a912c1e0feaff5e1
Joneyviana <arcistiel@gmail.com>
2016-09-09 08:45:41 -0300

    {msg}

--- tasks.py
+++ tasks.py
@@ -1,7 +1,15 @@
 
-from invoke import task
+from invoke import task, call
 
 @task
 def add(ctx):
     ctx.run("git add .")
 
+@task(add)
+def commit(ctx,msg):
+    ctx.run("git commit -m'{{msg}}'".format(msg))
+
+@task(pre=[call(commit, msg="new commit")])
+def deploy(ctx):
+    ctx.run("git push origin master")
+    ctx.run("git push heroku master")
